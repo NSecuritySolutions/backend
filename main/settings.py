f466a11943d66c16637edf3d57d7e5af37b3b800
Maruf995 <mamadalievmaruf740@gmail.com>
2024-04-01 21:44:08 +0600

    update settings

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-shp-qj=((702t^_b%g*+05pwwja=t!j7kv7*ge1y0l)h$#xk)3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'http://video.store', 'video.store']
 
 
 # Application definition
